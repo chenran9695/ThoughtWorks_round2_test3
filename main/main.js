c6584980fdb8c72aa6572433a2bbf6e25b5317db
chenran <chenran_uestc@163.com>
2019-05-29 23:52:16 +0800

    优化了第三个测试需求：more than 8km and parking的代码

--- main/main.js
+++ main/main.js
@@ -1,16 +1,34 @@
-function getParkingFee(parkTime) {
-    return parkTime * 0.25;
+//起步价（元）
+var basicFee = 6;
+//停车费（元/min）
+var parkPrice = 0.25;
+//2公里至8公里的收费（元/每公里）
+var priceBetweent2To8 = 0.8;
+// 8公里以后的加价倍率
+var rate = 0.5;
+// 8公里以外的收费（元/公里）
+var priceMoreThan8 = priceBetweent2To8 * (1+rate);
+
+function getParkFee(parkTime) {
+    return parkTime * parkPrice;
+}
+
+function getFeeBetween2To8(distance) {
+    return distance * priceBetweent2To8;
+}
+
+function getFeeMoreThan8(distance){
+    return distance*priceMoreThan8;
 }
 
 module.exports = function main(inputs) {
-    if(inputs.distance <= 2){
-        return 6+getParkingFee(inputs.parkTime);
+    let fee = basicFee+getParkFee(inputs.parkTime);
+    if(inputs.distance>=2 & inputs.distance<=8){
+        fee += getFeeBetween2To8(inputs.distance-2);
     }
-    else if(inputs.distance<=8){
-        return 6+(inputs.distance-2)*0.8+getParkingFee(inputs.parkTime);
+    else if(inputs.distance>8){
+        fee += (getFeeBetween2To8(6)+getFeeMoreThan8(inputs.distance-8));
     }
-    else{
-        return 6+(inputs.distance-8)*0.8*1.5+6*0.8+getParkingFee(inputs.parkTime);
-    }
+    return fee;
 };
 
