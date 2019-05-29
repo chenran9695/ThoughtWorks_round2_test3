da1ca16ae381f20ee2c22eaabd786d5c6ed2ba68
chenran <chenran_uestc@163.com>
2019-05-29 23:11:53 +0800

    完成了第二个测试需求：between 2 km and 8km and parking

--- main/main.js
+++ main/main.js
@@ -1,6 +1,13 @@
+function getParkingFee(parkTime) {
+    return parkTime * 0.25;
+}
+
 module.exports = function main(inputs) {
     if(inputs.distance <= 2){
-        return 6+inputs.parkTime*0.25;
+        return 6+getParkingFee(inputs.parkTime);
+    }
+    else if(inputs.distance<=8){
+        return 6+(inputs.distance-2)*0.8+getParkingFee(inputs.parkTime);
     }
 };
 
